46891f7aad9d78e8b7afef7983c5f02f291017e0
Richard <sinr0202@gmail.com>
2015-12-28 00:03:27 -0500

    fixed routes for partials to render

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,19 @@
 class HomeController < ApplicationController
 
+	def dashboard
+		render layout: nil
+	end
+
+	def signin
+		render layout: nil
+	end
+
+	def signup
+		render layout: nil
+	end
+
+	def welcome
+		render layout: nil
+	end
+
 end
