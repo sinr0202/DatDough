3a5875b4eae62c3f0205bf4c17aa321e9081609a
Richard <sinr0202@gmail.com>
2016-01-01 00:55:10 -0500

    added confirms and errors to importing page

--- app/assets/javascripts/controllers/import_ctrl.js
+++ app/assets/javascripts/controllers/import_ctrl.js
@@ -10,32 +10,40 @@ App.controller('ImportController',
 	function($scope, Upload){
 
 	var vm = this;
-	var url = '/import'
+	var url = '/import';
 	vm.loading = false;
 	vm.processed = false;
+	vm.error = false;
 
 	vm.submit = function(){
-		console.log('submit clicked');
-		if(vm.csvFile){
-			vm.upload(vm.csvFile)
-		} else {
-			alert('select a file first!')
+		if(confirm('Upload '+vm.csvFile.name+'?')){
+			if(vm.csvFile){
+				vm.upload(vm.csvFile)
+			} else {
+				alert('select a file first!');
+			}
 		}
-	}
+		return;
+	};
 
 	vm.upload = function(file){
 		vm.processed = true;
 		vm.loading = true;
 		vm.uploading = true;
+		vm.error = false;
 		Upload.upload({
             url: url,
             data: {file: file}
         }).then(function (resp) {
-        	delete vm.file;
+			vm.file = false;
 			vm.loading = false;
+			alert("csv file upload successful");
             console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
         }, function (resp) {
 			vm.loading = false;
+			vm.error = resp.data.error;
+			console.log(vm.error);
+			alert("csv file upload unsuccessful");
             console.log('Error status: ' + resp.status);
         }, function (evt) {
             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
@@ -44,6 +52,7 @@ App.controller('ImportController',
             }
             console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
         });
+        return;
 	};
 
 
